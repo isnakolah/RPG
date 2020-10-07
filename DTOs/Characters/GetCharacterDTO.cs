8b992425588e94d43772db612f87fcb1a14df5d1
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-10-07 14:15:45 +0300

    Add GetChracterDTO

---
+++ DTOs/Characters/GetCharacterDTO.cs
@@ -0,0 +1,15 @@
+﻿using RPG.Models;
+
+namespace RPG.DTOs.Characters
+{
+    public class GetCharacterDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Frodo";
+        public int HitPoints { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Defence { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
+    }
+}
