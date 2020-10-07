700eed0cf55d3860f3b33829e3c6916279fb1f4e
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-10-07 13:04:44 +0300

    Create a character service and interface

---
+++ Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,12 @@
+﻿using RPG.Models;
+using System.Collections.Generic;
+
+namespace RPG.Services.CharacterService
+{
+    interface ICharacterService
+    {
+        List<Character> GetAllCharacters();
+        Character GetCharacterById(int id);
+        List<Character> AddCharacter(Character newCharacter);
+    }
+}
