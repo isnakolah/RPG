eba46734e16cf13ce4356cd819497d2e899c24e2
Daniel Nakolah <nakolahdaniel1@gmail.com>
2020-10-07 12:37:49 +0300

    Get method to get a character based on the id

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RPG.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RPG.Controllers
 {
@@ -7,11 +9,22 @@ namespace RPG.Controllers
     [Route("[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static Character knight = new Character();
+        private static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character { Id = 1,  Name = "Sam" }
+        };
 
+        [HttpGet("GetAll")]
         public IActionResult Get()
         {
-            return Ok(knight);
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
         }
     }
 }
